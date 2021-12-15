5885272d4276d9d2cbc7994dafd9e384f5994e6d
Sérgio Dias <up201704889@fe.up.pt>
2021-12-15 01:11:25 +0000

    feat: added React client template

---
+++ output/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={styles.title}>
+          Empty information system!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+const styles = {
+  title: {
+    fontWeight: 'bold',
+    fontSize: 25
+  },
+}
+
+export default App;
