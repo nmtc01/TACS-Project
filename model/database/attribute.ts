2d1638b0eda1cbdaeec5d5c9966a85e87dd79976
tiagogsilva <tiago.silva.99@hotmail.com>
2021-12-20 17:16:07 +0000

    Some changes to the model

--- model/database/attribute.ts
+++ model/database/attribute.ts
@@ -1,9 +1,13 @@
-type type_ = "text" | "number" | "date";
+import type { AttributeJSON, type_ } from './types'
 
 export default class Attribute {
-    constructor(private name: string, private type: type_, private references?: number) {}
+    constructor(private name: string, private type: type_, private references?: string) {}
+    
+    static deserialize(input: AttributeJSON): Attribute {
+        return new Attribute(input.name, input.type, input.references);
+    }
 
     print() {
-        console.log(`\t${this.name}: ${this.type}` + this.references? `\treferences ${this.references}` : '');
+        console.log(`\t${this.name}: ${this.type}` + (this.references? `\treferences ${this.references}` : ''));
     }
 }
--- parser.ts
+++ parser.ts
@@ -1,5 +1,7 @@
-const config = require('./config.json');
+const config = require('./config/config.json');
 import Database from './model/database/database';
+import { TableJSON } from './model/database/types';
+
 
 function main() {
     console.log("Initializing parser...");
@@ -7,10 +9,15 @@ function main() {
 
     const database = new Database();
 
-    config.database.forEach(table => {
-        database.addTable(table.name, table.attributes);
+    config.database.forEach((table: TableJSON) => {
+        database.addTable(table);
     });
-
+    console.log("TYPEOf")
+    database.tables.forEach(element => {
+        element.attributes.forEach(att => {
+            
+        })
+    });
     database.print();
 
 }
